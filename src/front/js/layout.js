4871c0d5c1a4963a96f2c4b10fd89e456a54f8ff
david9833 <dagalisteo@gmail.com>
2023-01-07 10:31:12 +0000

    pagina de registro hecha

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -11,6 +11,7 @@ import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
 import { Login } from "./pages/Login.jsx";
+import { SignUp } from "./pages/SignUp.jsx";
 
 //create your first component
 const Layout = () => {
@@ -29,6 +30,7 @@ const Layout = () => {
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
                         <Route element={<Login/>} path="/login"/>
+                        <Route element={<SignUp/>} path="/signup" />
                     </Routes>
                     <Footer />
                 </ScrollToTop>
