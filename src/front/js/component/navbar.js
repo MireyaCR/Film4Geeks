2f45713174f1f946e00d10b9f547bb3dfdc4175b
MireyaCR <mireya.mcr@gmail.com>
2023-01-10 17:52:22 +0000

    navbar01

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,24 +1,25 @@
  
 import React from "react";
 import { Link } from "react-router-dom";
-
+import logo from "../../img/LOGO.png";
 
 //include your index.scss file into the bundle
 import "../../styles/navbar.css";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-expand-lg fixed-top">
-			<a className="navbar-brand p-2" href="#">Film4Geeks</a>
+		<nav className="navbar navbar-expand-sm fixed-top">
+           <a className="navbar-brand p-2" href="#">
+            <img src={logo} width={50} height={50}/>Film4Geeks</a>                
 			<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon">
-		<i className="fas fa-bars"></i>
-	  </span>
-    </button>
+             <span className="navbar-toggler-icon">
+		        <i className="fas fa-bars"></i>
+	         </span>
+            </button>
            
-           <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+           <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent" role="button">
                <a className="navbar-brand"></a>
-                   <ul className="navbar-nav mr-auto mt-2 mt-lg-0" href="#">
+                   <ul className="navbar-nav" href="#">
                    <li className="nav-item active">
                        <a className="nav-link" href="#">Home<span className="sr-only">(current)</span></a>
                    </li>
