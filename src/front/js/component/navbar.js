347e49d894ba683604db0e4f01664d22f9e67143
MireyaCR <mireya.mcr@gmail.com>
2023-01-10 11:49:09 +0000

    logo

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,38 @@
+ 
 import React from "react";
 import { Link } from "react-router-dom";
 
+
+//include your index.scss file into the bundle
+import "../../styles/navbar.css";
+
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
+		<nav className="navbar navbar-expand-lg fixed-top">
+			<a className="navbar-brand p-2" href="#">Film4Geeks</a>
+			<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon">
+		<i className="fas fa-bars"></i>
+	  </span>
+    </button>
+           
+           <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+               <a className="navbar-brand"></a>
+                   <ul className="navbar-nav mr-auto mt-2 mt-lg-0" href="#">
+                   <li className="nav-item active">
+                       <a className="nav-link" href="#">Home<span className="sr-only">(current)</span></a>
+                   </li>
+                   <li className="nav-item ">
+                       <a className="nav-link " href="#">Profile</a>
+                   </li>
+                   <li className="nav-item">
+                       <a className="nav-link" href="#">Search</a>
+                   </li>
+                   <li className="nav-item">
+                       <a className="nav-link" href="#">Starring</a>
+                   </li>              
+               </ul>
+           </div>
 		</nav>
 	);
 };
