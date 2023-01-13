ef9eed7e1e54bda91abd4bb469d54b32dc547756
david9833 <dagalisteo@gmail.com>
2023-01-13 20:33:35 +0000

    diagrama hecho

--- src/front/js/pages/Profile.jsx
+++ src/front/js/pages/Profile.jsx
@@ -11,83 +11,82 @@ export const Profile = () => {
 
     const {store, actions } = useContext(Context);
     const [genres, setGenres] = useState([])
-    const [prueba, setPrueba] = useState([])
- 
+    const [categories, setCategories] = useState([])
+    const [percentages, setPercentages] =useState([])
+   
     const typesGenres = {
-        "action":28,
-        "adventure":12,
-        "animation":16,
-        "comedy":35,
-        "crime":80,
-        "documentary":99,
-        "drama":18,
-        "family":10751,
-        "fantasy":14,
-        "history":36,
-        "horror":27,
-        "music":10402,
-        "mistery":9648,
-        "romance":10749,
-        "science-fiction":878,
-        "TV movie":10770,
-        "thriller":53,
-        "war":10752,
-        "western":37
+        28:"action",
+        12:"adventure",
+        16:"animation",
+        35:"comedy",
+        80:"crime",
+        99:"documentary",
+        18:"drama",
+        10751:"family",
+        14:"fantasy",
+        36:"history",
+        27:"horror",
+        10402:"music",
+        9648:"mistery",
+        10749:"romance",
+        878:"science-fiction",
+        10770:"TV movie",
+        53:"thriller",
+        10752:"war",
+        37:"western"
     }
 
     console.log("esto es array>>>>>>>>>",store.seen)
 
-    const contador = []
     const funcion=()=>{
-       
+        let objetoCategorias = {}
+        let categoryNames = []
+        let percentageArray = []
+
         for(let i = 0; i<store.seen.length; i++) {
-            for(let j=0; j<store.seen[i].length;j++){
-            }
-            contador.push(store.seen[i][1])
-            
-        }
-        return contador
-    }
-
-    const funcion2 = (array) => {
-        const array3 = array.join()
-        return array3
-    }
-    const var4 = [funcion2(prueba)]
-    console.log("esto es el var4",var4)
-
-    const funcion3 = (array) => {
-        for(let i=0; i<array.length; i++) {
-            if(array[i].includes("16")) {
-                return "animation"
+            if(store.seen[i][1] in objetoCategorias) {
+                objetoCategorias[store.seen[i][1]] = objetoCategorias[store.seen[i][1]] +1
+            }else {
+                objetoCategorias[store.seen[i][1]] = 1 
             }
         }
+        console.log("categorias valor diagrama",objetoCategorias)
+
+        let arrayCategories= Object.keys(objetoCategorias)
+        console.log("arraycategories",arrayCategories)
+
+        for(let i=0; i<arrayCategories.length; i++) {
+            let category = arrayCategories[i]
+            //generar array de nombres categorias
+            if(category in typesGenres ) {
+               categoryNames.push(typesGenres[category])
+            }else{
+                categoryNames.push("Genre " + category)
+            }
+            //generar array de porcentajes
+            let appearCount = objetoCategorias[category]
+            let percentage = appearCount*100/store.seen.length
+            percentageArray.push(percentage)
+        }
+        setCategories(categoryNames)
+        setPercentages(percentageArray)
     }
 
-    const var5 =funcion3(var4)
-    
     useEffect(()=> {
         setGenres(Object.keys(typesGenres))
-        setPrueba(funcion())
-        
+        funcion()    
     },[store.seen])
 
-    console.log("genres",genres)
-    console.log("store.seen",store.seen)
 
-  console.log("esto es prueba",prueba)
-  console.log("este es var5",var5)
-
- 
+    console.log("categorias>>", categories)
 
     const data= {
-        labels: [var5],
+        labels: categories,
         datasets:[{   //porcentaje de cada uno e los parametros
-            data:[75.65, 80, 87,99 ],  //porcentajes
-            backgroundColor: ["green","red","yellow","#D29435"] //fondo
+            data:percentages,  //porcentajes
+            backgroundColor: ["green","red","blue","orange","purple","brown","grey","pink"] //fondo
         }]
     }
-
     const options= {
         responsive:true
     }
@@ -100,19 +99,23 @@ export const Profile = () => {
                     <h3 className="line">Name: <h4 style={{color:"white"}} >David</h4></h3>
                     <h3 className="line">Email: <h4 style={{color:"white"}}>dagalisteo@gmail.com</h4></h3>
                 </div>
+
                 <div>
                     <h3>Your favourite Genders:</h3>
                 <Pie className="mt-3" data={data} options={options} />
                 </div>
             </div>
+
             <div>
                 <h3>My favourite films</h3>
                 <MyFavourites/>
             </div>
+
             <div>
                 <h3>Films I want to see</h3>
                 <PendingMovies/>
             </div>
+            
             <div>
                 <h3>Films watched</h3>
                 <FilmsWatched/>
