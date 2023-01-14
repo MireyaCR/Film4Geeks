08869d6ef6c58b72455369965d7e67532153840e
MireyaCR <mireya.mcr@gmail.com>
2023-01-14 20:06:51 +0000

    boton infierno

--- src/front/js/pages/Detailspopular.jsx
+++ src/front/js/pages/Detailspopular.jsx
@@ -16,11 +16,11 @@ const Detailspopular = () => {
             <h5 className="mb-3">{store.movies[params.index].title}</h5>
          </div> 
          <div className="row border-rounded position-relative"style={{width: "400px", height: "500px"}}>
-            <img className="img-fluid" src={`https://image.tmdb.org/t/p/w500/${store.movies[params.index].poster_path}`} alt="image1" style={{width: "100%", height: "100%"}} />
+            <img className="img-fluid border-rounded" src={`https://image.tmdb.org/t/p/w500/${store.movies[params.index].poster_path}`} alt="image1" style={{width: "100%", height: "100%"}} />
             <button className="btn-sm rounded-pill border-warning fas fa-play position-absolute" style={{
                 position: "absolute",
-                top: "90%",
-                left: "18%",
+                top: "7%",
+                left: "87%",
                 transform: "translate(-50%, -50%)",
                 zIndex: "1",
                 width:"3em",
@@ -28,44 +28,29 @@ const Detailspopular = () => {
             }} />
         </div>
 
-            <div className="row">
+            <div className="row" style={{width: "400px", height: "500px"}}>
                 <div >
-                    <img src="" alt="Ver mas tarde" />
-                    <img src="" alt="Visto" />
-                    <img src="" alt="Favorito" />    
-                </div>iconos de la izquierda       
-                        <p><small>Duración:</small></p>Duración pelicula derecha
+                    <span className="fas fa-check-circle ml-2 p-1 float-left"></span>
+                    <span className="fas fa-star ml-2 p-1 float-left"></span>
+                    <span className="fas fa-flag ml-2 p-1 float-left"></span>  
+                     
+                    <span className="far fa-clock p-1 float-right"></span>                
                 </div>
-            <div className="row ms-2"> sinopsis y estrellas
-                <h1>SINOPSIS</h1>
-                <div>
-                    <button>
-                        <i className="fas fa-star">e1</i>
-                    </button>
-                    <button>
-                        <i className="fas fa-star">e2</i>
-                    </button>
-                    <button>
-                        <i className="fas fa-star">e3</i>
-                    </button>
-                    <button>
-                        <i className="fas fa-star">e4</i>
-                    </button>
-                    <button>
-                        <i className="fas fa-star">e5</i>
-                    </button>
-                </div>estrellas de la derecha        
+            </div>
+            <div className="row ms-2"> 
+                <h6>SYNOPSIS</h6>                
             </div>
             <div className="row ms-2">
                 <p>{store.movies[params.index].overview}</p> 
-                <h6>CLASIFICACION</h6>
-                <p>generos: terror, comedia</p>
-                <h6>DIRECTOR</h6>
-                <p>nombre del director</p>
-                <h6>REPARTO</h6>
+                <h6>FILM RATING</h6>
+                <p>clasificación</p>
+                <h6>GENRE</h6>                
                 <p>nombre de los actores</p>
-                <button>Critica(activo cuando haya escrito)</button>
-                <input></input>
+                <h6>DIRECTOR:</h6>
+                <p>nombre del director</p>
+                <h6>CASTING</h6>
+                <p>nombre de los actores</p>
+                
             </div>            
 	</div>
     );
