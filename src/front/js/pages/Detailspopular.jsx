f93e867d7d99094c8575b25ec7fdc52610f39254
MireyaCR <mireya.mcr@gmail.com>
2023-01-15 19:33:55 +0000

    dando color

--- src/front/js/pages/Detailspopular.jsx
+++ src/front/js/pages/Detailspopular.jsx
@@ -26,24 +26,24 @@ const Detailspopular = () => {
 
 	return (
         <div className="container mt-3">        
-          <div className="row">
-            <div className="col-md-6">
+          <div className="row justify-content-center">
+            <div className="col-md-6 col-12 p-1">
               <div className="row-image border-rounded position-relative">
                 <img className="img-fluid" src={`${API_IMAGE}${popularMovie.poster_path}`} alt="image1" />
                 <button className="play-button fas fa-play"></button>
               </div>
               <div className="row gutter">
-                <div className="p-0 d-flex align-items-center">
+                <div className="d-flex p-1">
                   <span className="fas fa-check-circle mr-3"></span>
                   <span className="fas fa-star mr-3"></span>
                   <span className="fas fa-flag mr-3"></span> 
-                  <span className="far fa-clock mr-3"> {popularMovie.runtime} min. </span>                
+                  <span className="far fa-clock"> {popularMovie.runtime} min. </span>                
                 </div>
               </div>
             </div>
-            <div className="col-md-6">
+            <div className="col-md-6 col-12 p-1">
               
-              <div className="row reduced-line-height text-border-shine">
+              <div className="p-1 reduced-line-height text-border-shine">
                 <h5 className="mt-1 text-color-h5">{popularMovie.title}</h5>
                 <p><small className="text-color-small">SYNOPSIS</small></p>   
                 <p>{popularMovie.overview}</p> 
