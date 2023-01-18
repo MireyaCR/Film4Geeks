6a0fd2433fe783eb90377a6a980eb150303f4194
MireyaCR <mireya.mcr@gmail.com>
2023-01-18 18:02:49 +0000

    ajustando las cards

--- src/front/js/pages/Search.jsx
+++ src/front/js/pages/Search.jsx
@@ -1,60 +1,93 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "../../styles/search.css";
 import { API_IMAGE } from "/workspace/Film4Geeks/src/front/js/services/API_IMAGE.js";
 
+function Search() {
+  const [searchValue, setSearchValue] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-function Search(){
-const [searchValue, setSearchValue] = useState('');
-const [searchResults, setSearchResults] = useState([]);
-const [isLoading, setIsLoading] = useState(false);
-const [error, setError] = useState(null);  
-
-const search = () => { 
-
+  const search = () => {
     const apiKey = process.env.TMDB_API;
     setIsLoading(true);
 
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchValue}&language=en-US&page=1&include_adult=false`)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        if(data.error){
-            setError(data.error);
-        }else{
-            setSearchResults(data.results);
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchValue}&language=en-US&page=1&include_adult=false`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (data.error) {
+          setError(data.error);
+        } else {
+          setSearchResults(data.results);
         }
-         setIsLoading(false);
+        setIsLoading(false);
       })
-      .catch(error => {
+      .catch((error) => {
         setError(error);
         setIsLoading(false);
-      }); 
-  };  
+      });
+  };
+
+   const sinopsys = (result) => {
+    const overview = result.overview;
+    return overview.length > 120 ? overview.substr(0, 120) + "..." : overview;
+  };
+
   return (
-    <div>
-        <div className="d-flex justify-content-center">
-          <input type="text" className="rounded text-black" placeholder="Please enter your query" aria-label="Recipient's username" value={searchValue} onChange={e => setSearchValue(e.target.value)}/>
-          <button className="btn btn-custom rounded-pill shadow" type="button" onClick={search}>Search</button>
-          {isLoading && <div>Loading...</div>}
-          {error && <div>{error.message}</div>}
-        </div> 
-        <div className="d-flex flex-wrap flex-row">    
-            {searchResults.length > 0 ? (
-              <div>{searchResults.map(result => (<div key={result.id} className="card result-card m-3">
-                   <div className="card-container d-flex justify-content-end"> 
-                       <img className="card-img-left" width ="150px" height= "200px"src={`${API_IMAGE}${result.poster_path}`} alt=""></img>
-                      <div className="card-body ml-auto">
-                        <h5 className="card-title-right ">{result.title}</h5>
-                        <p className="cadr-text-right text-warning">{result.release_date}</p>
-                        <p className="card-text-right text-warning">{result.overview}</p>
-                      </div> 
-                    </div>            
+    <div className="container mx-auto">
+      <div className="d-flex justify-content-center mt-4 mb-3">
+            <input type="text" className="ms-3 rounded-pill text-center text-warning text-sm inputclass" placeholder="Please enter your query" aria-label="Recipient's username" value={searchValue}    
+              onChange={(e) => setSearchValue(e.target.value)}
+              onKeyUp={(e) => e.key === "Enter" && search()}
+            />
+            <button
+              className="btn btn-custom rounded-pill btn-shadow text-center text-sm"type="button" onClick={()=>searchValue && search()}disabled={!searchValue}>Search
+            </button>
+            {isLoading && <div>Loading...</div>}
+            {error && <div>{error.message}</div>}
+      </div>
+      <div className="d-flex flex-wrap flex-row">
+        {searchResults.length > 0 ? (
+          <div className="card-deck mx-auto">
+            {searchResults.map((result) => (
+              <div key={result.id} className="card result-card m-2 ">
+                <div
+                  className="card-container d-flex justify-content-end colores"
+                  style={{ height: "200px" }}
+                >
+                  <img
+                    className="img-fluid "
+                    src={`${API_IMAGE}${result.poster_path}`}
+                    style={{
+                      objectFit: "cover",
+                      width: "150px",
+                      height: "200px",
+                      maxWidth: "100%",
+                      maxHeight: "200px",
+                    }}
+                    alt=""
+                  ></img>
+                  <div className="card-body ml-auto">
+                    <h5 className="card-title-right ">{result.title}</h5>
+                    <p className="cadr-text-right text-warning">
+                      {result.release_date}
+                    </p>
+                    <p className="card-text-right text-warning">
+                      {sinopsys(result)}
+                    </p>
                   </div>
-                ))}
+                </div>
               </div>
-            ):'No results found'}
+            ))}
           </div>
+        ) : (
+          ""
+        )}
+      </div>
     </div>
-      )
- }
+  );
+}
 export default Search;
