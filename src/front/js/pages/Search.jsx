1f46436e32438d15e7e1403f6cfd701267027e17
MireyaCR <mireya.mcr@gmail.com>
2023-01-19 10:57:01 +0000

    finalizando la busqueda

--- src/front/js/pages/Search.jsx
+++ src/front/js/pages/Search.jsx
@@ -31,67 +31,77 @@ function Search() {
       });
   };
 
-   const sinopsys = (result) => {
+  const sinopsys = (result) => {
     const overview = result.overview;
     return overview.length > 120 ? overview.substr(0, 120) + "..." : overview;
   };
 
   return (
     <div className="container mx-auto">
-      <div className="d-flex flex-row-sm justify-content-center mt-4 mb-3">
-        <div className="col-sm-6">
-            <input type="text" className="ms-5 rounded-pill text-center text-warning text-sm inputclass" placeholder="Please enter your query" aria-label="Recipient's username" value={searchValue}    
+      <form>
+        <div className="form-group">
+          <div className="input-group mt-3">
+            <input
+              type="text"
+              className="form-control rounded-input"
+              placeholder="Please enter your query"
+              value={searchValue}
               onChange={(e) => setSearchValue(e.target.value)}
               onKeyUp={(e) => e.key === "Enter" && search()}
             />
-        </div>
-        <div className="col-sm-6">
             <button
-              className="btn btn-custom rounded-pill btn-shadow text-center text-sm"type="button" onClick={()=>searchValue && search()}disabled={!searchValue}>Search
+              className="btn btn-rounded btn-orange-gradient"
+              onClick={() => searchValue && search()}
+              disabled={!searchValue}
+            >
+              Search
             </button>
             {isLoading && <div>Loading...</div>}
             {error && <div>{error.message}</div>}
           </div>
-      </div>
-      <div className="d-flex flex-wrap flex-row">
-        {searchResults.length > 0 ? (
-          <div className="card-deck mx-auto">
-            {searchResults.map((result) => (
-              <div key={result.id} className="card result-card m-2 ">
-                <div
-                  className="card-container d-flex justify-content-end colores"
-                  style={{ height: "200px" }}
-                >
-                  <img
-                    className="img-fluid "
-                    src={`${API_IMAGE}${result.poster_path}`}
-                    style={{
-                      objectFit: "cover",
-                      width: "150px",
-                      height: "200px",
-                      maxWidth: "100%",
-                      maxHeight: "200px",
-                    }}
-                    alt=""
-                  ></img>
-                  <div className="card-body ml-auto">
-                    <h5 className="card-title-right ">{result.title}</h5>
-                    <p className="cadr-text-right text-warning">
-                      {result.release_date}
-                    </p>
-                    <p className="card-text-right text-warning">
-                      {sinopsys(result)}
-                    </p>
+        </div>
+        <div className="d-flex flex-wrap flex-row">
+          {searchResults.length > 0 ? (
+            <div className="card-deck mx-auto">
+              {searchResults.map((result) => (
+                <div key={result.id} className="card result-card m-2 ">
+                  <div
+                    className="card-container d-flex justify-content-end colores"
+                    style={{ height: "200px" }}
+                  >
+                    <img
+                      className="img-fluid "
+                      src={`${API_IMAGE}${result.poster_path}`}
+                      style={{
+                        objectFit: "cover",
+                        width: "150px",
+                        height: "200px",
+                        maxWidth: "100%",
+                        maxHeight: "200px",
+                      }}
+                      alt=""
+                    ></img>
+                    <div className="card-body ml-auto">
+                      <h5 className="card-title-right ">{result.title}</h5>
+                      <p className="cadr-text-right text-warning">
+                        {result.release_date}
+                      </p>
+                      <p className="card-text-right text-warning">
+                        {sinopsys(result)}
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        ) : (
-          ""
-        )}
-      </div>
+              ))}
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
+      </form>
     </div>
   );
 }
 export default Search;
+
+
