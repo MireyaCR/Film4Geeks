a283f3e9e0b711752611064674694e4ac4cf5753
MireyaCR <mireya.mcr@gmail.com>
2023-01-20 12:59:34 +0000

    wellcome

--- src/front/js/component/Wellcome.jsx
+++ src/front/js/component/Wellcome.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Well from "../../img/Well.png"
+import Well from "../../img/Well.png";
+import "/workspace/Film4Geeks/src/front/styles/wellcome.css";
 
 const Wellcome = () => {
-    return(
-        <>
-        <img className="img-fluid" src={Well} alt="" style={{
-            objectFit: 'cover',
-            width: '100%',
-            height: '100vh'
-        }} />
-
-        <div className="btn-container">
-            <Link to="/login"><button>Login</button></Link>
-            <Link to="/signup"><button>Signup</button></Link>
-        </div>
-        </>
-    )
-}
-export default Wellcome; 
+  return (
+    <div>
+      <div className="container">
+        <img
+          className="img-fluid"
+          src={Well}
+          alt=""
+          style={{ objectFit: "cover", height: "100vh" }}
+        />
+      </div>
+      <div className="btn-container-well">
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/signup">
+          <button>Signup</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+export default Wellcome;
