bb38fc003bdf9b280235ef6eeb82eebb007dc9a1
MireyaCR <mireya.mcr@gmail.com>
2023-01-23 20:39:44 +0000

    finalizando 2

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -5,8 +5,8 @@ import React, { Component} from "react";
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+			Made with <i className="fa fa-heart text-danger" /> &💥🥵 by Jack, David & Mireya{" "}
+			<a href="http://www.4geeksacademy.com"></a>
 		</p>
 	</footer>
 );
