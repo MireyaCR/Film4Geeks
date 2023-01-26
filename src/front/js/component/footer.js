035e225adea0135d28b0ff088ffd6e1d913cdc1d
MireyaCR <mireya.mcr@gmail.com>
2023-01-26 16:49:14 +0000

    busqueda fin 2

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,9 @@
 import React, { Component} from "react";
 
 
-
 export const Footer = () => (
 	<div >
-			<p className="text-info text-center mt-4" style={{fontSize: '15px'}}> Made with ❤️💥🥵 by Jack & David & Mireya</p><br/>		
+			<p className="text-center mt-4" style={{fontSize: '15px',color:'#1c9eb8',fontWeight:'bold'}}> Made with ❤️💥🥵 by Jack & David & Mireya</p><br/>		
 			<a href="http://www.4geeksacademy.com"></a>
 			
 	</div>
