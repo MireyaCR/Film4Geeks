b526c385015b97be9d6475fb1e435d2b3a7f088b
MireyaCR <mireya.mcr@gmail.com>
2023-02-21 21:03:32 +0000

    environment vars

--- render_build.sh
+++ render_build.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
+touch .env
+echo "BASENAME=$BASENAME" >> .env
+echo "DATABASE_URL=$DATABASE_URL" >> .env
+cat .env
+
+exit 1;
 
 npm install
 npm run build
